9d1521c5887a52dd04d282c19986837ea20d6dbd
sarojlimbu0731 <sarojlimbu0731@gmail.com>
2023-01-28 01:29:41 +0545

    footer component added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import './App.css';
+import "./App.css";
 
-import { Header } from './components/Header';
-import {HeaderSlider} from './components/HeaderSlider';
-import CollegeList from './components/CollegeList';
-import CountryList from './components/CountryList';
-import TestPrepList from './components/TestPrepList';
-import CareerAndSubscription from './components/CareerAndSubscription';
-
+import { Header } from "./components/Header";
+import { HeaderSlider } from "./components/HeaderSlider";
+import CollegeList from "./components/CollegeList";
+import CountryList from "./components/CountryList";
+import TestPrepList from "./components/TestPrepList";
+import CareerAndSubscription from "./components/CareerAndSubscription";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-        <Header/>
-        <HeaderSlider />
-        <CollegeList/>
-        <CountryList/>
-        <TestPrepList/>
-        <CareerAndSubscription/>
+      <Header />
+      <HeaderSlider />
+      <CollegeList />
+      <CountryList />
+      <TestPrepList />
+      <CareerAndSubscription />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div className="mt-5">
+      <div className="footerContainer">
+        <div className="footer">
+          <div className="fLists">
+            <ul className="fList">
+              <li className="fListItem">Countries</li>
+              <li className="fListItem">Regions</li>
+              <li className="fListItem">Districts</li>
+              <li className="fListItem">Cities</li>
+            </ul>
+            <ul className="fList">
+              <li className="fListItem">Countries</li>
+              <li className="fListItem">Regions</li>
+              <li className="fListItem">Districts</li>
+              <li className="fListItem">Cities</li>
+            </ul>
+            <ul className="fList">
+              <li className="fListItem">Countries</li>
+              <li className="fListItem">Regions</li>
+              <li className="fListItem">Districts</li>
+              <li className="fListItem">Cities</li>
+            </ul>
+            <ul className="fList">
+              <li className="fListItem">Countries</li>
+              <li className="fListItem">Regions</li>
+              <li className="fListItem">Districts</li>
+              <li className="fListItem">Cities</li>
+            </ul>
+            <ul className="fList">
+              <li className="fListItem">Countries</li>
+              <li className="fListItem">Regions</li>
+              <li className="fListItem">Districts</li>
+              <li className="fListItem">Cities</li>
+            </ul>
+          </div>
+          <div className="fText">Copyright@ 2023 Global StudyCenter Pvt. Ltd.</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
